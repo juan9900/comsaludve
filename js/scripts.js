14720dc3cf2febc1a63722cc18e02443f6e5980a
juan9900 <juanluislauretta@gmail.com>
2023-03-10 11:20:06 -0400

    added responsive and two modals

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $('#modal-plan-2').modal('show');
+    $('#plan-1').on('click', function(){
+        $('#modal-plan-1').modal('show');
+    })
+    $('#plan-2').on('click', function(){
+        $('#modal-plan-2').modal('show');
+    })
+    $('#plan-3').on('click', function(){
+        $('#modal-plan-3').modal('show');
+    })
+    $('#plan-4').on('click', function(){
+        $('#modal-plan-4').modal('show');
+    })
+})
